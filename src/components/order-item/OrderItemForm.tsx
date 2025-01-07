ba869b1c61e971e00ef3e6a914a205dadd99d5f1
Ali Haider <devalihaider@gmail.com>
2025-01-07 16:21:58 +0500

    create itemform

--- src/components/order-item/OrderItemForm.tsx
+++ src/components/order-item/OrderItemForm.tsx
@@ -1,9 +1,118 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Input, Switch, Typography} from "@material-tailwind/react";
 
-function OrderItemForm() {
+interface Props {
+    selectedItem: {
+        id: string;
+        name: string;
+        description: string | null;
+        piece: number;
+        price: {
+            dry_clean: string | null;
+            wash: string | null;
+        };
+        defaults: {
+            cleaning_method: "wash" | "dry_clean";
+            handling_option: "fold" | "hang";
+        };
+    }
+}
+
+interface ItemPayload {
+    is_open_item: false;
+    item_id: string;
+    quantity: number | null;
+    cleaning_method: "wash" | "dry_clean" | null;
+    handling_option: "fold" | "hang" | null;
+    price_per_unit: number | null;
+}
+
+const OrderItemForm: React.FC<Props> = ({selectedItem}) => {
+
+    const [formData, setFormData] = useState<ItemPayload>({
+        is_open_item: false,
+        item_id: selectedItem.id,
+        quantity: null,
+        cleaning_method: selectedItem.defaults.cleaning_method,
+        handling_option: selectedItem.defaults.handling_option,
+        price_per_unit: selectedItem.defaults.cleaning_method === "wash" ? Number(selectedItem.price.wash) : Number(selectedItem.price.dry_clean),
+    })
     return (
         <div>
+            <Typography
+                variant="small"
+                color="blue-gray"
+                className="mt-4 text-left font-medium"
+            >
+                Selected Item Details:
+            </Typography>
+            <br/>
+            <div className="grid grid-cols-2 gap-2 ">
 
+                <Input
+                    crossOrigin="crossOrigin"
+                    label="Name"
+                    value={selectedItem.name || ""}
+                    readOnly
+                />
+                <Input
+                    crossOrigin="crossOrigin"
+                    label="Description"
+                    value={selectedItem.description || "No Description"}
+                    readOnly
+                />
+            </div>
+            <br/>
+            <div className="grid grid-cols-2 gap-2 ">
+                <Input
+                    crossOrigin="crossOrigin"
+                    label="Cleaning Method"
+                    value={selectedItem.defaults.cleaning_method || "No Default Cleaning Method"}
+                    readOnly
+                />
+                <Input
+                    crossOrigin="crossOrigin"
+                    label="Handling Option"
+                    value={selectedItem.defaults.handling_option || "No Default Handling Option"}
+                    readOnly
+                />
+            </div>
+            <br/>
+            <Input
+                crossOrigin="crossOrigin"
+                type="number"
+                label="Quantity"
+                required
+                value={formData.quantity || ""}
+                onChange={(e) =>
+                    setFormData({
+                        ...formData,
+                        quantity: e.target.value === "" ? null : Number(e.target.value),
+                    })
+                }
+            /><br/>
+            <Input
+                crossOrigin="crossOrigin"
+                type="number"
+                label="Price per piece"
+                required
+                value={formData.price_per_unit || ""}
+                onChange={(e) =>
+                    setFormData({
+                        ...formData,
+                        price_per_unit: e.target.value === "" ? null : Number(e.target.value),
+                    })
+                }
+            />
+            <br/>
+            <label>
+                <Switch
+                    disabled
+                    crossOrigin={`crossOrigin`}
+                    checked={formData.is_open_item}
+                />
+                &nbsp;Is Order Open
+            </label>
         </div>
     );
 }
--- src/components/order-item/SelectItemFromDropDown.tsx
+++ src/components/order-item/SelectItemFromDropDown.tsx
@@ -2,20 +2,49 @@ import React, {useState} from 'react';
 import {config} from "@/config";
 import useFetch from "@/lib/api/Dashboard/hooks/area/useFetchAreas";
 import {Typography} from "@material-tailwind/react";
+import OrderItemForm from "@/components/order-item/OrderItemForm";
 
 interface Props {
     categoryId: string | null
 }
 
+interface ItemPayload {
+    is_open_item: false;
+    item_id: string;
+    quantity: number;
+    cleaning_method: "wash" | "dry_clean";
+    handling_option: "fold" | "hang";
+    price_per_unit: number;
+}
+
+interface Item {
+    id: string;
+    name: string;
+    description: string | null;
+    piece: number;
+    price: {
+        dry_clean: string | null;
+        wash: string | null;
+    };
+    defaults: {
+        cleaning_method: "wash" | "dry_clean";
+        handling_option: "fold" | "hang";
+    };
+}
+
 const SelectItemFromDropDown: React.FC<Props> = ({categoryId}) => {
     const url = `${config.BASE_URL}/items?category_id=${categoryId}`
     const {data, error, loading, refetch} = useFetch<any>(url);
-    const [selectedItems, setSelectedItems] = useState<string | null>();
+    // const [selectedItems, setSelectedItems] = useState<Item | null>();
+    const [selectedItemId, setSelectedItemId] = useState<string | null>(null);
 
     const handleItemChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const categoryId = e.target.value;
-        setSelectedItems(categoryId);
+        const itemId = e.target.value;
+        setSelectedItemId(itemId);
+
     };
+    const selectedItem = data?.result?.find((item: { id: string }) => item.id === selectedItemId);
+
     return (
         <>
             <div>
@@ -30,7 +59,7 @@ const SelectItemFromDropDown: React.FC<Props> = ({categoryId}) => {
                     <p className="text-red-500 text-xs">Error: {error}</p>) : data?.result?.length > 0 ? (
                     <select
                         className="p-2 rounded w-full border border-gray-400"
-                        value={selectedItems || ""}
+                        value={selectedItemId || ""}
                         onChange={handleItemChange}
                     >
                         <option value="" disabled>
@@ -39,12 +68,14 @@ const SelectItemFromDropDown: React.FC<Props> = ({categoryId}) => {
                         {data.result.map((item: { id: string; name: string }) => (
                             <option key={item.id} value={item.id}>
                                 {item.name}
-                            </option>))}
+                            </option>
+                        ))}
                     </select>) : (<p className="text-gray-600">No item available.</p>)}
             </div>
-            {selectedItems && <div>
+            {selectedItem && (
+                <OrderItemForm selectedItem={selectedItem}/>
+            )}
 
-            </div>}
         </>);
 }
 
